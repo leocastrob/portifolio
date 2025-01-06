a47b829d4521fb73e8c70911754d7a09319798ec
Leocastrob <leo_078@hotmail.com>
2025-01-06 12:09:37 -0300

    Criação do componente do navbar para navegação entre as seçoes da pagina

---
+++ src/componentes/NavBar.tsx
@@ -0,0 +1,23 @@
+import { AppBar, MenuItem, Toolbar, styled } from "@mui/material"
+
+
+const NavBar = () => {
+    const StyledToobar = styled(Toolbar)(() => ({
+        display: "flex",
+        justifyContent: "space-evenly"
+    }))
+
+    return (
+        <>
+            <AppBar position="absolute">
+                <StyledToobar>
+                    <MenuItem>Sobre</MenuItem>
+                    <MenuItem>Skills</MenuItem>
+                    <MenuItem>Projetos</MenuItem>
+                </StyledToobar>
+            </AppBar>
+        </>
+    )
+}
+
+export default NavBar
