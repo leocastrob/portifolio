0c8de08c68f209c0e63c6acded711be9262653b0
Leocastrob <leo_078@hotmail.com>
2025-01-06 12:08:25 -0300

    Criação do componente de estilização do Botão

---
+++ src/componentes/Botao.tsx
@@ -0,0 +1,38 @@
+import { styled } from "@mui/material"
+import theme from "../tema"
+import { ReactNode } from "react"
+
+interface StylebuttonProps {
+  children: ReactNode
+}
+
+const Stylebutton: React.FC<StylebuttonProps> = ({ children }) => {
+
+    const Stylebutton = styled("button")(()=> ({
+        backgroundColor: "transparent",
+        border: `1px solid ${theme.palette.primary.contrastText}`,
+        borderRadius : "3px",
+        padding: "5px 15px",
+        width: "100%",
+        color: theme.palette.primary.contrastText,
+        display: "inline-flex",
+        alignItems : "center",
+        justifyContent: "center",
+        gap: "10px",
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.light
+        }
+         
+     }))
+
+    return (
+      <>
+      <Stylebutton>
+        {children}
+      </Stylebutton>
+      </>
+    )
+  }
+  
+  export default Stylebutton
+  
