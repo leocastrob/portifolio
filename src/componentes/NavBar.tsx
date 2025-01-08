2b784fb99aa48604d4b178b090ba54a141e3651e
Leocastrob <leo_078@hotmail.com>
2025-01-08 08:36:07 -0300

    feita o mecanismo de rolagem do site direto da navbar

--- src/componentes/NavBar.tsx
+++ src/componentes/NavBar.tsx
@@ -1,25 +1,24 @@
-import { AppBar, MenuItem, Toolbar, styled } from "@mui/material"
-
+import { AppBar, MenuItem, Toolbar, styled } from "@mui/material";
 
 const NavBar = () => {
-    const StyledToobar = styled(Toolbar)(() => ({
-        display: "flex",
-        justifyContent: "space-evenly", 
-        width: "100vw", 
-        overflowX: "hidden"
-    }))
+  const StyledToobar = styled(Toolbar)(() => ({
+    display: "flex",
+    justifyContent: "space-evenly",
+    width: "100vw",
+    overflowX: "hidden",
+  }));
 
-    return (
-        <>
-            <AppBar position="absolute">
-                <StyledToobar>
-                    <MenuItem>Sobre</MenuItem>
-                    <MenuItem>Skills</MenuItem>
-                    <MenuItem>Projetos</MenuItem>
-                </StyledToobar>
-            </AppBar>
-        </>
-    )
-}
+  return (
+    <>
+      <AppBar position="absolute">
+        <StyledToobar>
+          <MenuItem component="a" href="#sobre">Sobre</MenuItem>
+          <MenuItem component="a" href="#skills">Skills</MenuItem>
+          <MenuItem component="a" href="#projetos">Projetos</MenuItem>
+        </StyledToobar>
+      </AppBar>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
